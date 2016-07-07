afe215b45869af37464aebd808f688ca81c4e637
Attila Szász <tilusnet@gmail.com>
2016-07-07 09:33:33 +0100

    Logging facility, testing a call.

--- meetup-album-downloader.py
+++ meetup-album-downloader.py
@@ -1,9 +1,16 @@
 import meetup_api_client
+import logging
 
+
+def set_root_loglevel(level):
+    logging.getLogger().setLevel(level)
 
 def get_client():
     name, config = meetup_api_client.app.get_config()
     return meetup_api_client.app.get_client(config)
 
+
 if __name__ == '__main__':
+    set_root_loglevel(logging.DEBUG)
     meetup_client = get_client()
+    j_result = meetup_client.get_photos()
